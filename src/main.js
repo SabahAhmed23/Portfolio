6dc9fcbeb79a7d447ef0e3ad3b5a3b77ceed3a26
SabahAhmed <159125737+SabahAhmed23@users.noreply.github.com>
2024-02-13 20:56:26 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require(".src/styles.css");
+import ".src/styles.css";
+
 document.addEventListener('DOMContentLoaded', function () {
-    var interBubble = document.querySelector('.interactive');
-    var curX = 0;
-    var curY = 0;
-    var tgX = 0;
-    var tgY = 0;
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
     function move() {
         curX += (tgX - curX) / 20;
         curY += (tgY - curY) / 20;
-        interBubble.style.transform = "translate(".concat(Math.round(curX), "px, ").concat(Math.round(curY), "px)");
-        requestAnimationFrame(function () {
-            move();
-        });
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(move);
     }
+
     window.addEventListener('mousemove', function (event) {
         tgX = event.clientX;
         tgY = event.clientY;
     });
+
     move();
 });
+
