a8cd8d50c41bef52806966c054d80eb48e8e740d
SabahAhmed <159125737+SabahAhmed23@users.noreply.github.com>
2024-02-13 22:11:32 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//import "../src/styles.css";
+import "../src/styles.css";
 
 document.addEventListener('DOMContentLoaded', function () {
     const interBubble = document.querySelector('.interactive');
